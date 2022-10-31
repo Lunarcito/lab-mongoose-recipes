1419ed14dfa2700e55c4dd09f5039423f7402f9c
Carolina Calle <calle.carolina@gmail.com>
2022-10-31 17:42:20 +0100

    :Lab solved

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
+/*
+
 mongoose
   .connect(MONGODB_URI)
   .then(x => {
@@ -21,3 +22,81 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  */
+
+//Iteration 1: Connection to the database "recipe-app"
+
+const connectToMongo = async function() {
+  try {
+    const x = await mongoose.connect(MONGODB_URI )
+    console.log( `conected to ${x.connection.name}`)
+    createRecipe()
+    addRecipes ()
+    updateRecipe()
+    deleteRecipe()
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+//Iteration 2: Create a recipe
+
+const createRecipe = async function () {  
+  try{
+    const myReceipe = await Recipe.create({
+      title: "PizzaC",
+      level: "Easy Peasy",
+      ingredients: ["Extra virgin olive oil","Cornmeal","Tomato sauce","Firm mozzarella cheese","Fresh soft mozzarella cheese","Parmesan cheese","Feta cheese"],
+      cuisine: "Italian",
+      dishType: "main_course",
+      image: "",
+      duration: 30,
+      creator: "Carolina"
+    })
+    //console.log(myReceipe)
+  
+  } catch(err){
+    console.log('ERROR: ', err)
+  }
+
+}
+//Iteration 3: Insert Multiple Recipes
+const addRecipes = async function () {  
+  try{
+    const newRecipes = await Recipe.insertMany(data)
+    for (let i =0; i< newRecipes.length;i++){
+      console.log (newRecipes[i].title)
+    }
+
+  } catch(err){
+    console.log('ERROR: ', err)
+  }
+} 
+
+
+//Iteration 4: Update Recipes
+
+const updateRecipe = async function () {  
+  try{
+    const updatedRecipes = await Recipe.updateOne({title:"Rigatoni alla Genovese"},{duration:100});
+    console.log (`Has been fixed ${updatedRecipes.title}`)
+  } catch(err){
+    console.log('ERROR: ', err)
+  }
+} 
+
+
+//Iteration : Delete a Recipe
+
+const deleteRecipe = async function () {  
+  try{
+    const deletedRecipes = await Recipe.deleteOne({title: Carrot Cake});
+    console.log ("Deleted")
+
+} catch(err){
+    console.log('ERROR: ', err)
+  }
+} 
+
+connectToMongo();
